e9c0a205d21250251694e62cf6757e6c0ee46d91
Sr.Dayran <dayranrs2015@gmail.com>
2019-05-11 13:59:30 -0300

    primeiro commit

---
+++ luz.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lampada;
+
+/**
+ *
+ * @author darai
+ */
+public class luz {
+    private boolean estado;
+    
+    public luz(){
+        estado=false;
+    }
+    public String getestado(){
+        if(estado){
+            return "ACESSA";
+        }else{
+                return "APAGADA";
+                }
+    }
+    public void mudarestado(){
+        this.estado=!this.estado;
+    }
+   
+}
+
+
+
